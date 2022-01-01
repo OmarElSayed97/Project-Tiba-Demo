2a1144aea3a216c8da00e2299be4d6c1aaf1a7f3
Steven <stevennassef97@gmail.com>
2022-01-01 20:40:13 +0200

    Importing DOTween

---
+++ Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModulePhysics2D.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9f61584b1643aab3096d13442e43d7cd2d0af3e4697ab736f8be6670c707c9a1
+size 12213
---
+++ Assets/Plugins/Demigiant/DOTween/Modules/DOTweenModuleUtils.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b1a26d699d872363f9c36e4e9ac1820fde3f452290b573b3555453ec24296295
+size 6718
